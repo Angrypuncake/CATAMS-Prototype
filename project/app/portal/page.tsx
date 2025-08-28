cafff53118333176aef81f4c020c2465dfcb3eb9
Paul Zhang <hzha8009@uni.sydney.edu.au>
2025-08-28 13:16:53 +1000

    feat(frontend) : [IS-22] Add primary MUI button style

--- project/app/portal/page.tsx
+++ project/app/portal/page.tsx
@@ -4,36 +4,16 @@ const page = () => {
   return (
     <div className="flex flex-col gap-2">
       <h1>Portal Page</h1>
-      <Button
-        type="link"
-        href="/dashboard/tutor"
-        variant="outlined"
-        sx={{ borderColor: "black", textTransform: "none", width: "300px" }}
-      >
+      <Button type="link" href="/dashboard/tutor" variant="primary">
         Tutor Dashboard
       </Button>
-      <Button
-        type="link"
-        href="/dashboard/assistent"
-        variant="outlined"
-        sx={{ borderColor: "black", textTransform: "none", width: "300px" }}
-      >
+      <Button type="link" href="/dashboard/assistent" variant="primary">
         Teaching assistant Dashboard
       </Button>
-      <Button
-        type="link"
-        href="/dashboard/coordinator"
-        variant="outlined"
-        sx={{ borderColor: "black", textTransform: "none", width: "300px" }}
-      >
+      <Button type="link" href="/dashboard/coordinator" variant="primary">
         Coordinator Dashboard
       </Button>
-      <Button
-        type="link"
-        href="/dashboard/admin"
-        variant="outlined"
-        sx={{ borderColor: "black", textTransform: "none", width: "300px" }}
-      >
+      <Button type="link" href="/dashboard/admin" variant="primary">
         System Admin Dashboard
       </Button>
     </div>
--- project/theme/theme.ts
+++ project/theme/theme.ts
@@ -3,14 +3,32 @@
 import type { ThemeOptions } from "@mui/material/styles";
 import { createTheme } from "@mui/material/styles";
 
+declare module "@mui/material/Button" {
+  interface ButtonPropsVariantOverrides {
+    primary: true;
+    secondary: true;
+  }
+}
+
 const themeOptions: ThemeOptions = {
   components: {
     MuiButton: {
-      styleOverrides: {
-        root: {
-          borderRadius: "12px",
+      variants: [
+        {
+          props: { variant: "primary" },
+          style: ({ theme }) => ({
+            color: theme.palette.common.black,
+            boxShadow: theme.shadows[8],
+            border: "1.5px solid black",
+            textTransform: "none",
+            fontWeight: 500,
+            "&:hover": {
+              filter: "brightness(110%)",
+              boxShadow: theme.shadows[1],
+            },
+          }),
         },
-      },
+      ],
     },
   },
 };

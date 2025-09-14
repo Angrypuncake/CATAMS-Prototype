6cf55f36ce8628b55bd76c3c58d1c9a8d304c389
Paul(Hanqi) Zhang <hzha8009@uni.sydney.edu.au>
2025-09-14 17:27:01 +1000

    feat(frontend) : [IS-5] Created a Login Form that only checks username

--- project/app/page.tsx
+++ project/app/page.tsx
@@ -2,7 +2,5 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/portal");
-  // For the sake of demoing, redirect to /portal.
-  // In the final product this should redirect to /login
+  redirect("/login");
 }

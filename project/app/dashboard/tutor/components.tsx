70a0d74955d54dfb9186ce270885d6da84a71fb2
Justin Hoogwaerts <jhoo9750@uni.sydney.edu.au>
2025-09-16 07:53:56 +1000

    feat(frontend) : [IS-999] Beautified tutor dashboard page

--- project/app/dashboard/tutor/components.tsx
+++ project/app/dashboard/tutor/components.tsx
@@ -1,13 +1,22 @@
 import React, { ReactNode } from "react";
+import clsx from "clsx";
 
 type StyledBoxProps = {
   children: ReactNode;
+  accentColor?: string;
 };
 
-const StyledBox: React.FC<StyledBoxProps> = ({ children }) => (
-  <div className="w-full border shadow-md bg-white border-black rounded-lg p-6 my-5 mx-0">
-    {children}
-  </div>
-);
+const StyledBox: React.FC<StyledBoxProps> = ({ children, accentColor }) => {
+  return (
+    <div
+      className={clsx(
+        "w-full shadow-md bg-white rounded-lg p-6 my-5 mx-0 border",
+        accentColor ? `border-l-4 ${accentColor}` : "border-black",
+      )}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default StyledBox;

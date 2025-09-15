ac457e60daab6367043c23f22e2f20c2f6f1aed9
Paul(Hanqi) Zhang <hzha8009@uni.sydney.edu.au>
2025-09-16 08:17:03 +1000

    feat(frontend) : [IS-5] Remove reduntant code and styling

--- project/app/login/page.tsx
+++ project/app/login/page.tsx
@@ -7,44 +7,38 @@ import { Button, TextField, Typography } from "@mui/material";
 export default function LoginPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    setIsLoading(true);
 
     try {
       // TODO connect to backend
     } catch (error) {
       console.log(error);
-    } finally {
-      setIsLoading(false);
     }
   };
 
   return (
     <div className="min-h-screen flex w-full items-center justify-center bg-gray-50">
-      <div className="max-w-lg w-full mx-4 space-y-8 bg-white p-8 rounded-lg shadow-lg">
-        <div>
-          <Typography
-            variant="h4"
-            component="h2"
-            align="center"
-            fontWeight="bold"
-            sx={{ mt: 3, mb: 3 }}
-          >
-            Sign in to your account
-          </Typography>
-        </div>
-        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+      <div className="max-w-lg w-full mx-4 space-y-8 bg-white p-8 rounded-lg shadow-lg flex flex-col gap-3">
+        <Typography
+          variant="h4"
+          component="h2"
+          align="center"
+          fontWeight="bold"
+        >
+          Sign in to your account
+        </Typography>
+        <form
+          className="mt-8 space-y-6 flex flex-col gap-3"
+          onSubmit={handleSubmit}
+        >
           <div className="flex flex-col gap-2">
             <TextField
-              id="username"
               name="username"
               type="text"
               required
-              fullWidth
               label="Username"
               placeholder="Enter your username"
               value={username}
@@ -52,11 +46,9 @@ export default function LoginPage() {
               variant="outlined"
             />
             <TextField
-              id="password"
               name="password"
               type="password"
               required
-              fullWidth
               label="Password"
               placeholder="Enter your password"
               value={password}
@@ -64,28 +56,16 @@ export default function LoginPage() {
               variant="outlined"
             />
           </div>
-
-          <div>
-            <Button
-              type="submit"
-              disabled={isLoading}
-              variant="contained"
-              fullWidth
-              sx={{ mt: 3, mb: 2 }}
-            >
-              {isLoading ? "Signing in..." : "Sign in"}
-            </Button>
-          </div>
-          <div>
-            <Button
-              variant="outlined"
-              fullWidth
-              onClick={() => router.push("/portal")}
-              sx={{ mt: 2 }}
-            >
-              Go to Portal
-            </Button>
-          </div>
+          <Button type="submit" variant="contained" fullWidth>
+            Sign in
+          </Button>
+          <Button
+            variant="outlined"
+            fullWidth
+            onClick={() => router.push("/portal")}
+          >
+            Go to Portal
+          </Button>
         </form>
       </div>
     </div>

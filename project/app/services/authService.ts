5959a72017d714788b4ce4424dfa2491ce682fff
Elvis Nguyen <engu9651@uni.sydney.edu.au>
2025-10-12 16:43:10 +1100

    feat(backend) : [IS-141] allowed submission of claim from tutor allocation view

--- project/app/services/authService.ts
+++ project/app/services/authService.ts
@@ -2,6 +2,6 @@ import axios from "@/lib/axios";
 import { CurrentUser } from "../_types/user";
 
 export async function getUserFromAuth() {
-  const res = await axios.get("/me");
+  const res = await axios.get("auth/me");
   return res.data as CurrentUser;
 }

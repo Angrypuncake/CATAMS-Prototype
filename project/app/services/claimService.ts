0678bd29d1e81308578d06b756b55b26c47534af
Elvis Nguyen <engu9651@uni.sydney.edu.au>
2025-10-12 11:35:03 +1100

    feat(backend) : [IS-141] Created service function for creating a claim

---
+++ project/app/services/claimService.ts
@@ -0,0 +1,7 @@
+import axios from "@/lib/axios";
+import { Claim, CreateClaimPayload } from "../_types/claim";
+
+export async function createClaim(payload: CreateClaimPayload): Promise<Claim> {
+  const res = await axios.post("/api/claims", payload);
+  return res.data.data as Claim;
+}

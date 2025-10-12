283c9b3cd511418a9108fa1fcfb0cc7b86fce1c9
Elvis Nguyen <engu9651@uni.sydney.edu.au>
2025-10-12 13:37:38 +1100

    feat(backend) : [IS-141] moved mock data out of uc dashboard

--- project/app/dashboard/coordinator/page.tsx
+++ project/app/dashboard/coordinator/page.tsx
@@ -8,36 +8,7 @@ import CoordinatorApprovalTable from "./CoordinatorApprovalTable";
 import AlertBox from "@/components/AlertBox";
 import Link from "next/link";
 import axios from "axios";
-
-const pendingRequests = [
-  {
-    requestID: "REQ001",
-    type: "Swap",
-    relatedSession: "CS101 - 2025-09-10 2:00 PM",
-    status: "Pending",
-    creator: "John Doe",
-    creatorRole: "Tutor",
-    user_id: 123,
-  },
-  {
-    requestID: "REQ003",
-    type: "Swap",
-    relatedSession: "BIO105 - 2025-09-14 1:00 PM",
-    status: "Pending",
-    creator: "Jane Smith",
-    creatorRole: "TA",
-    user_id: 456,
-  },
-  {
-    requestID: "REQ005",
-    type: "Swap",
-    relatedSession: "PHYS110 - 2025-09-13 10:00 AM",
-    status: "Pending",
-    creator: "Alice Johnson",
-    creatorRole: "Tutor",
-    user_id: 789,
-  },
-];
+import { pendingRequests } from "./mock";
 
 const Page = () => {
   // State for dropdown
